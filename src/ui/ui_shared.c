librelous/librelous

commit 1833a3d6f77ea73762b07148e00e55a9a4b42804

* Fix text alignment of UI items that display cvar contents * (bug 3952) display winning team string at the bottom of the scoreboard   after a game * Prevent users from changing some ui communication cvars
git-svn-id: svn://svn.icculus.org/tremulous/trunk@1173 fa00571f-6d07-0410-8660-cfe97aa2585a

src/ui/ui_shared.c
@@ -4469,15 +4469,15 @@ void Item_SetTextExtents( itemDef_t *item, int *width, int *height, const char *
   *width = item->textRect.w;
   *height = item->textRect.h;
 
+  // as long as the item isn't dynamic content (ownerdraw or cvar), this
   // keeps us from computing the widths and heights more than once
-
-  if( *width == 0 || ( item->type == ITEM_TYPE_OWNERDRAW && item->textalignment == ALIGN_CENTER ) )
+  if( *width == 0 || item->cvar || ( item->type == ITEM_TYPE_OWNERDRAW &&
+      item->textalignment != ALIGN_LEFT ) )
   {
     int originalWidth;
 
-    if( item->type == ITEM_TYPE_EDITFIELD && item->textalignment == ALIGN_CENTER && item->cvar )
+    if( item->cvar && item->textalignment != ALIGN_LEFT )
     {
-      //FIXME: this will only be called once?
       char buff[256];
       DC->getCVarString( item->cvar, buff, 256 );
       originalWidth = UI_Text_Width( item->text, item->textscale, 0 ) +
@@ -4900,6 +4900,7 @@ void Item_Text_Wrapped_Paint( itemDef_t *item )
         itemDef_t   lineItem;
         int         width, height;
 
+        memset( &lineItem, 0, sizeof( itemDef_t ) );
         strncpy( buff, p, lineLength );
         buff[ lineLength ] = '\0';
         p = &textPtr[ i + 1 ];

src/cgame/cg_main.c
@@ -290,13 +290,14 @@ static cvarTable_t cvarTable[ ] =
   
   { &cg_debugVoices, "cg_debugVoices", "0", 0 },
 
-  { &ui_currentClass, "ui_currentClass", "0", 0 },
-  { &ui_carriage, "ui_carriage", "", 0 },
-  { &ui_stages, "ui_stages", "0 0", 0 },
-  { &ui_dialog, "ui_dialog", "Text not set", 0 },
-  { &ui_voteActive, "ui_voteActive", "0", 0 },
-  { &ui_humanTeamVoteActive, "ui_humanTeamVoteActive", "0", 0 },
-  { &ui_alienTeamVoteActive, "ui_alienTeamVoteActive", "0", 0 },
+  // communication cvars set by the cgame to be read by ui
+  { &ui_currentClass, "ui_currentClass", "0", CVAR_ROM },
+  { &ui_carriage, "ui_carriage", "", CVAR_ROM },
+  { &ui_stages, "ui_stages", "0 0", CVAR_ROM },
+  { &ui_dialog, "ui_dialog", "Text not set", CVAR_ROM },
+  { &ui_voteActive, "ui_voteActive", "0", CVAR_ROM },
+  { &ui_humanTeamVoteActive, "ui_humanTeamVoteActive", "0", CVAR_ROM },
+  { &ui_alienTeamVoteActive, "ui_alienTeamVoteActive", "0", CVAR_ROM },
 
   { &cg_debugRandom, "cg_debugRandom", "0", 0 },
   

src/ui/ui_main.c
@@ -123,6 +123,7 @@ static cvarTable_t    cvarTable[ ] =
   { &ui_textWrapCache, "ui_textWrapCache", "1", CVAR_ARCHIVE },
   { &ui_developer, "ui_developer", "0", CVAR_ARCHIVE | CVAR_CHEAT },
   { &ui_emoticons, "cg_emoticons", "1", CVAR_LATCH | CVAR_ARCHIVE },
+  { &ui_winner, "ui_winner", "", CVAR_ROM }
 };
 
 static int    cvarTableSize = sizeof( cvarTable ) / sizeof( cvarTable[0] );

ui/teamscore.menu
@@ -356,5 +356,19 @@
       ownerdraw CG_SPECTATORS
       decoration
     }
+
+    itemDef
+    {
+      name winner
+      rect (W/2) (H+BAR_H) 0 0
+      style WINDOW_STYLE_EMPTY
+      forecolor 1 1 1 1
+      textscale 0.5
+      textalign ALIGN_CENTER
+      textvalign VALIGN_TOP
+      visible MENU_TRUE
+      cvar "ui_winner"
+      decoration
+    }
   }
 }

commit 64b1355ceec64f1091eb05b51143b3b9693bbc2c

* Prevent option controls and edit fields from twitching bizarrely when used   (caused by r1173)
git-svn-id: svn://svn.icculus.org/tremulous/trunk@1204 fa00571f-6d07-0410-8660-cfe97aa2585a

src/ui/ui_shared.c
@@ -4443,6 +4443,15 @@ void Rect_ToWindowCoords( rectDef_t *rect, windowDef_t *window )
 void Item_SetTextExtents( itemDef_t *item, int *width, int *height, const char *text )
 {
   const char *textPtr = ( text ) ? text : item->text;
+  qboolean cvarContent;
+
+  // it's hard to make a policy on what should be aligned statically and what
+  // should be aligned dynamically; there are reasonable cases for both. If
+  // it continues to be a problem then there should probably be an item keyword
+  // for it; but for the moment only adjusting the alignment of ITEM_TYPE_TEXT
+  // seems to suffice.
+  cvarContent = ( item->cvar && item->textalignment != ALIGN_LEFT &&
+                  item->type == ITEM_TYPE_TEXT );
 
   if( textPtr == NULL )
     return;
@@ -4452,15 +4461,15 @@ void Item_SetTextExtents( itemDef_t *item, int *width, int *height, const char *
 
   // as long as the item isn't dynamic content (ownerdraw or cvar), this
   // keeps us from computing the widths and heights more than once
-  if( *width == 0 || item->cvar || ( item->type == ITEM_TYPE_OWNERDRAW &&
+  if( *width == 0 || cvarContent || ( item->type == ITEM_TYPE_OWNERDRAW &&
       item->textalignment != ALIGN_LEFT ) )
   {
     int originalWidth;
 
-    if( item->cvar && item->textalignment != ALIGN_LEFT )
+    if( cvarContent )
     {
-      char buff[256];
-      DC->getCVarString( item->cvar, buff, 256 );
+      char buff[ MAX_CVAR_VALUE_STRING ];
+      DC->getCVarString( item->cvar, buff, sizeof( buff ) );
       originalWidth = UI_Text_Width( item->text, item->textscale, 0 ) +
                       UI_Text_Width( buff, item->textscale, 0 );
     }